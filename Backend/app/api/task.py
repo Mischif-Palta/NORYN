59d9393536fd7080b99928dfe086af4bf9457ff6
Mischif-Palta <chaitanyapalta@gmail.com>
2026-08-18 21:54:39 +0530

    feat(tasks): complete task CRUD

--- Backend/app/api/task.py
+++ Backend/app/api/task.py
@@ -3,11 +3,27 @@ from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.core.dependencies import get_current_user
 from app.models.user import User
-from app.schemas.task import CreateTask, ResponseTask
-from app.services.task_service import create_task
+from app.schemas.task import CreateTask, ResponseTask, UpdateTask
+from app.services.task_service import create_task, get_tasks, get_task, update_task, delete_task
 
 router = APIRouter(prefix = "/tasks", tags = ["Tasks"])
 
 @router.post("/", response_model = ResponseTask)
 def create_new_task(task_data : CreateTask, db : Session = Depends(get_db), current_user : User = Depends(get_current_user)):
     return create_task(task_data, current_user, db)
+
+@router.post("/", response_model = list[ResponseTask])
+def get_all_tasks(db : Session = Depends(get_db), current_user : User = Depends(get_current_user)):
+    return get_tasks(current_user, db)
+
+@router.post("/{task_id}", response_model = ResponseTask)
+def get_single_task(task_id: int, db : Session = Depends(get_db), current_user : User = Depends(get_current_user)):
+    return get_task(task_id, current_user, db)
+
+@router.put("/{task_id}", response_model = ResponseTask)
+def update_existing_task(task_id : int, task_data : UpdateTask ,db : Session = Depends(get_db), current_user : User = Depends(get_current_user)):
+    return update_task(task_id, task_data, current_user, db)
+
+@router.delete("/{task_id}")
+def delete_existing_task(task_id = int, db : Session = Depends(get_db), current_user : User = Depends(get_current_user)):
+    return delete_task(task_id, current_user, db)
--- Backend/app/services/task_service.py
+++ Backend/app/services/task_service.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from app.models.task import Task
 from app.models.user import User
 from app.schemas.task import CreateTask, UpdateTask
+from fastapi import HTTPException, status
 
 def create_task(task_data : CreateTask, current_user : User, db : Session):
     new_task = Task(title = task_data.title, description = task_data.description, priority = task_data.priority,
@@ -11,3 +12,47 @@ def create_task(task_data : CreateTask, current_user : User, db : Session):
     db.refresh(new_task) #gives latest version of the current row
     return new_task
 
+def get_tasks(current_user, db):
+    tasks = db.query(Task).filter(Task.user_id == current_user.id).all()
+    return tasks
+
+def get_task(task_id, current_user, db):
+    task = db.query(Task).filter(Task.id == task_id, Task.user_id == current_user.id).first()
+
+    if task is None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Task not found!")
+    
+    return task
+
+def update_task(task_id, task_data, current_user, db):
+    task = db.query(Task).filter(Task.id == task_id, Task.user_id == current_user.id).first()
+
+    if task is None:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Task not found!")
+
+    if task_data.title is not None:
+        task.title = task_data.title
+
+    if task_data.description is not None:
+            task.description = task_data.description
+
+    if task_data.status is not None:
+            task.status = task_data.status
+
+    if task_data.priority is not None:
+            task.priority = task_data.priority
+
+    if task_data.due_date is not None:
+            task.due_date = task_data.due_date
+
+    db.commit()
+    db.refresh(task)
+    return task
+
+def delete_task(task_id, current_user, db):
+     task = db.query(Task).filter(Task.id == task_id, Task.user_id == current_user.id).first()
+     if task is None:
+          raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Task not found!")
+     db.delete(task)
+     db.commit()
+     return {"message" : "Task deleted successfully!"}
